3d8d006c9028f58cfd88c1ae288d79f17ec8b49f
Pradip Baniya <pradipbaniya422@gmail.com>
2026-03-20 00:01:54 +0545

    Start My Account page with a simple layout shell

---
+++ Nepal-Cozy-Care-frontend/src/pages/MyAccount.tsx
@@ -0,0 +1,14 @@
+import Layout from "../components/layout/Layout";
+
+export default function MyAccount() {
+  return (
+    <Layout>
+      <section className="account-page">
+        <div className="account-container">
+          <h1>My Account</h1>
+          <p>Manage your profile, orders, addresses, and preferences.</p>
+        </div>
+      </section>
+    </Layout>
+  );
+}
